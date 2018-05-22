685888205aea4ade27e953f55b30943a8b000ad7
hosein74 <hosein.z.74@gmail.com>
2018-05-22 17:15:06 +0430

    edit a connection.php

--- connection.php
+++ connection.php
@@ -5,22 +5,21 @@
  */
 class DB
 {
-  private static $host ;
-  private static $database ;
-  private static $username ;
-  private static $password ;
+  public static $host = "localhost";
+  public static $database = "resturant" ;
+  public static $username = "root";
+  public static $password = "";
 
-  private static $instance = NULL;
-  function __construct($h="localhost",$db = "res",$un="root",$p=""){
-    $host = $h;
-    $database = $db;
-    $username = $un;
-    $password = $p;
+  private static $instance = null;
+  function __construct(){
+  }
+  function __clone()
+  {
   }
 
-  public static function getInstance()
+    public static function getInstance()
   {
-    if (isset(self::$instance)) {
+    if (!isset(self::$instance)) {
       self::$instance = new PDO("mysql:host=".self::$host.";dbname=".self::$database.";charset=utf8",self::$username,self::$password) or die("connection error");
     }
     return self::$instance;
@@ -28,6 +27,5 @@ class DB
 
 }
 
-
-
  ?>
+
