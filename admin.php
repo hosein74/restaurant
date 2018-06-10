cd96c3f697d2c5c9189610e8ab30b2383a382c81
skeivannejad <skeivannejad@gmail.com>
2018-06-10 15:08:58 +0430

    s conflict

--- admin.php
+++ admin.php
@@ -43,6 +43,8 @@ else {
 }
 ?>
 <script src="node_modules/jquery/dist/jquery.min.js"></script>
+
+    <script src="node_modules/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
 <script>
 
    
@@ -77,14 +79,30 @@ else {
                         "            <div class=\"card-body\">\n" +
                         "                <h5 class=\"card-title\">"+value['product_name']+"</h5>\n" +
                         "                <p class=\"card-text\">"+value['product_cost']+"</p>\n" +
-                        "                <button class=\"btn btn-primary edit  \" ><?php echo $editfood ?></button>\n" +
+                        "                <button class=\"btn btn-primary edit  \" data-toggle='modal' data-target='#modalform' ><?php echo $editfood ?></button>\n" +
                         "                <label  hidden for=''>"+value['product_id']+"</label>\n" +
                         "				  <button class=\"btn btn-primary delete  \" ><?php echo $deletefood ?></button>\n"+
                         "                <label   hidden for=''>"+value['product_id']+"</label>\n" +
                         "            </div>\n" +
                         "        </div>")
                 });
+                $("#products").append("<div class=\"modal fade\" id=\"modalform\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalCenterTitle\" aria-hidden=\"true\">\n" +
+                    "                   <div class=\"modal-dialog modal-dialog-centered\" role=\"document\">" +
+                    "                       <div class=\"modal-content\">" +
+                    "                           <div class=\"modal-header\">" +
+                    "                               <h5 class=\"modal-title\" id=\"exampleModalLongTitle\">Modal title</h5>\n" +
+                    "                               <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n" +
+                    "                                       <span aria-hidden=\"true\">&times;</span>\n" +
+                    "                               </button>\n" +
+                    "                           </div>\n" +
+                    "                           <div class=\"modal-body\" id='modal-body'>\n" +
+                    "                                ...\n" +
+                    "                           </div>" +
+                    "                       </div>\n" +
+                    "                   </div>\n" +
+                    "               </div>")
 			})
+
             .fail(function (xhr, status, errorThrown) {
                 alert( "Sorry, there was a problem!" );
             })
@@ -99,15 +117,12 @@ else {
 			
 			url: "ajax-deleteproduct.php",
             data: {productid:id},
-            type: "GET",
+            type: "GET"
 			
 		})
 		
 		.done(function(){
-			alert(id);
-			
-			
-			
+
 		})
 		
 		.fail(function (xhr, status, errorThrown) {
@@ -125,10 +140,54 @@ else {
         $(document).on('click','.delete', function(){
             id = $(this).next().text();
 			deleteproduct(id);
+            card = $(this).parent().parent();
+            card.remove();
         });
         $(document).on('click','.edit', function(){
-            id = $(this).next().text();
-            
+            name = $(this).prev().prev().text();
+            cost = $(this).prev().text();
+            $("#exampleModalLongTitle").text("<?php echo $editfood?>");
+            $("#modal-body").empty();
+            $("#modal-body").append(
+                "        <form action='' id='editform' method=\"post\" enctype=\"multipart/form-data\">\n" +
+                "                    <div class=\"form-row\">\n" +
+                "                        <div class=\"form-group col-md-6\">\n" +
+                "                            <label for=\"foodname\"><?php echo $foodname?></label>\n" +
+                "                            <input name=\"foodname\" type=\"text\" value='"+name+"' class=\"form-control\" id=\"foodname\" aria-describedby=\"emailHelp\" placeholder=\"<?php echo $foodname?>\">\n" +
+                "                        </div>\n" +
+                "                        <div class=\"form-group col-md-6\">\n" +
+                "                            <label for=\"foodcost\"><?php echo $foodcost?></label>\n" +
+                "                            <input name=\"foodcost\" type=\"text\" value='"+cost+"' class=\"form-control\" id=\"foodcost\" aria-describedby=\"emailHelp\" placeholder=\"<?php echo $foodcost?>\">\n" +
+                "                        </div>\n" +
+                "                        <div class=\"form-group col-md-6\">\n" +
+                "                            <label for=\"picture\"><?php echo $picture?></label>\n" +
+                "                            <input type=\"file\" name=\"picture\" id=\"picture\">\n" +
+                "                        </div>\n" +
+                "                    </div>\n" +
+                "\n" +
+                "\n" +
+                "                <div class=\"form-group\">\n" +
+                "                    <button type=\"submit\" class=\"btn btn-primary\"><?php echo $editfood?></button>\n" +
+                "                </div>\n" +
+                "\n" +
+                "        </form>\n" +
+                "\n"
+                )
+        });
+        $(document).on('submit','editform',function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: "ajax-editproduct.php",
+                type: "POST",
+                data: new FormData(this),
+                contentType: false,
+                cache: false,
+                processData:false,
+                success: function(data)
+                {
+
+                }
+            });
         });
         getProduct(1,6);
     });
